bc118f0e19215251c5496709bf24c3f2d241146e
tommykeem <tommykeem@gmail.com>
2023-07-31 14:26:01 -0700

    Added Turns and Playable Board

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask_socketio import SocketIO, emit
 from flask_session import Session
+from tempfile import mkdtemp
 from uuid import uuid4
 
 app = Flask(__name__)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config['SECRET_KEY'] = uuid4().hex
-app.config['SESSION_PERMANENT'] = True
+app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = "filesystem"
 socketio = SocketIO(app)
 Session(app)
@@ -16,17 +18,52 @@ def connect():
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        if request.form.get('select') == "3x3":
-            session['board'] = 3
-        if request.form['select'] == "4x4":
-            session['board'] = 4
-        if request.form['select'] == "5x5":
-            session['board'] = 5
-        return render_template('game.html', board=session['board'])
-        
     return render_template('index.html')
 
+@app.route("/game", methods=['GET', 'POST'])
+def game():
+    print(session)
+    if request.method == "POST":
+        if request.form.get('select') == "3x3":
+            session['board_size'] = 3
+            session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+            session['turn'] = 'X'
+        if request.form.get('select') == "4x4":
+            session['board_size'] = 4
+            session["board"] = [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]]
+            session['turn'] = 'X'
+        if request.form['select'] == "5x5":
+            session['board_size'] = 5
+            session["board"] = [[None, None, None, None, None], [None, None, None, None, None], [None, None, None, None, None], [None, None, None, None, None], [None, None, None, None, None]]
+            session['turn'] = 'X'
+    return render_template('game.html', board_size=session['board_size'], board=session['board'], turn=session['turn'])
 
-def generate_board(board):
-    return
+@app.route("/play/<int:row>/<int:col>")
+def play(row, col):
+    session["board"][row][col] = session["turn"]
+    print(session)
+    if session['turn'] == 'X':
+        session['turn'] = 'O'
+    else:
+        session['turn'] = 'X'
+    return redirect(url_for('game'))
+
+@app.route("/clear")
+def clear():
+    if session["board_size"] == 3:
+        session["board"] = [[None,None,None],
+                            [None,None, None],
+                            [None,None, None]]
+    elif session["board_size"] == 4:
+        session["board"] = [[None, None, None, None], 
+                            [None, None, None, None], 
+                            [None, None, None, None], 
+                            [None, None, None, None]]
+    else:
+        session["board"] = [[None, None, None, None, None], 
+                            [None, None, None, None, None], 
+                            [None, None, None, None, None], 
+                            [None, None, None, None, None], 
+                            [None, None, None, None, None]]
+    session["turn"] = "X"
+    return redirect(url_for("game"))
